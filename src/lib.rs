348fd88672cb0df6c392b7bfc4872d1ff0468a7e
Daniël de Kok <me@danieldk.eu>
2019-11-20 11:26:06 +0100

    Make get_graph a constructor for TreeNode
    Also give it a more constructor-like name, create_tree, since that is
    what it does ;).

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,4 @@ extern crate lazy_static;
 extern crate failure;
 
 pub mod edit_tree;
-pub use crate::edit_tree::{get_graph, Apply, ToLowerCharVec, TreeNode};
+pub use crate::edit_tree::{Apply, ToLowerCharVec, TreeNode};
